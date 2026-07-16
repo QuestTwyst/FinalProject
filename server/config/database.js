214691bc8cd847029d90b31f46bc0fca326a238c
johannadev738 <jdevilme4882@eagle.fgcu.edu>
2026-07-16 19:22:17 -0400

    aditional information for database.js

---
+++ server/config/database.js
@@ -0,0 +1,24 @@
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { Pool } = pg;
+
+const pool = new Pool({
+  host: process.env.PGHOST,
+  port: Number(process.env.PGPORT || 5432),
+  database: process.env.PGDATABASE,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+});
+
+pool.on("connect", () => {
+  console.log("Connected to the QuestTwyst PostgreSQL database");
+});
+
+pool.on("error", (error) => {
+  console.error("Unexpected PostgreSQL error:", error);
+});
+
+export default pool;
