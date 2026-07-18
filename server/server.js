e77d350f16d1480874cbd2bb8e14bf7ad34823f0
johannadev738 <jdevilme4882@eagle.fgcu.edu>
2026-07-17 22:40:12 -0400

    Created routes and controllers for genres, readingProgress, storyGenres, storyHistory

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import storiesRoutes from "./routes/stories.js";
+import genresRouter from "./routes/genres.js";
+import storyGenresRouter from "./routes/storyGenres.js";
+import readingProgressRouter from "./routes/readingProgress.js";
+import storyHistoryRouter from "./routes/storyHistory.js";
 
 dotenv.config();
 
@@ -12,6 +16,10 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/stories", storiesRoutes);
+app.use("/api/genres", genresRouter);
+app.use("/api/stories", storyGenresRouter);
+app.use("/api/progress", readingProgressRouter);
+app.use("/api/story-history", storyHistoryRouter);
 
 app.get("/", (req, res) => {
   res.status(200).json({
